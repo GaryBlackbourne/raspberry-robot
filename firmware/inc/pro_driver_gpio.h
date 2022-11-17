a5e58097acf6a48c7913fd4760e5218fa29fb31d
Gergely Koloszar <gergely.koloszar@gmail.com>
2022-11-17 14:01:57 +0100

    Enabled alternate function and separated GPIO setup management into one function

--- firmware/inc/pro_driver_gpio.h
+++ firmware/inc/pro_driver_gpio.h
@@ -1,6 +1,6 @@
 #ifndef PRO_DRIVER_GPIO_H
 #define PRO_DRIVER_GPIO_H
 
-void init_gpio_a();
+void init_gpio();
 
 #endif
--- firmware/src/pro_driver_gpio.c
+++ firmware/src/pro_driver_gpio.c
@@ -1,9 +1,9 @@
 #include "pro_driver_gpio.h"
 #include "stm32f103xb.h"
 
-
-void init_gpio_a(){
-  RCC->APB2ENR |= RCC_APB2ENR_IOPAEN_Msk; // enable clock for GPIO A
+void init_gpio(){
+  RCC->APB2ENR |= RCC_APB2ENR_IOPAEN; // enable clock for GPIO A
+  RCC->APB2ENR |= RCC_APB2ENR_IOPBEN; // enable clock for GPIO B
+  RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
   return;
 }
-
