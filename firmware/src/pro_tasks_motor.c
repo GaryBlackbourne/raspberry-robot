68db3887887ec3dfdb452024c8adf6cd198f0e01
Gergely Koloszar <gergely.koloszar@gmail.com>
2022-11-17 14:04:23 +0100

    Add test motor task for pwm testing

--- firmware/src/pro_tasks_motor.c
+++ firmware/src/pro_tasks_motor.c
@@ -1,11 +1,21 @@
 #include "FreeRTOS.h"
-#include "task.h"
+#include "portmacro.h"
 
+#include "pro_driver_usart.h"
+
+#include "task.h"
 #include "pro_tasks_motor.h"
 
+#include "pro_driver_timer.h"
+
 void vTaskMotor(void* vp){
-  vTaskSuspend(NULL);
+
+  motor_right_set_speed(forward, 2500);
+  motor_left_set_speed(forward, 2500);
+
   while(1){
+    
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
 
