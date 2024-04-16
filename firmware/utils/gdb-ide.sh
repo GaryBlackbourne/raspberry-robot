5b71f2e3e7c5014e83673c4b1999b7d1a1cdcf89
Gergely Koloszar <gergely.koloszar@gmail.com>
2024-04-16 14:29:36 +0200

    Add gdb and openocd scripts

---
+++ firmware/utils/gdb-ide.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+PROJECT_ROOT=$(dirname "$0")/..
+source "$PROJECT_ROOT"/settings.sh
+
+if [ -f "$OUTPUT_DIR/program.elf" ]; then 
+    arm-none-eabi-gdb -i=mi -ex 'target extended-remote localhost:3333' "$OUTPUT_DIR"/program.elf
+else
+  printf "No binary in %s." "$OUTPUT_DIR"
+  exit 1
+fi
