05531f71c50aa68338f9e0aedfb86700a7418ddf
Gergely Koloszar <gergely.koloszar@gmail.com>
2024-04-29 19:09:15 +0200

    Add motor parameters and defines

---
+++ firmware/modules/main/inc/motor_parameters.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_MOTOR_PARAMETERS_H
+#define _MAIN_MOTOR_PARAMETERS_H
+
+#define PI                    (float)3.14159265359
+#define RADIUS                (float)0.021          // [m]
+#define DIAMETER              2*RADIUS       // [m]
+
+#define GEAR_RATIO            ((float)100/1)        // 100 motor round = 1 wheel round
+#define INCREMENTS_PER_ROUND  14
+
+#endif
